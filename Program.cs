ba11af9bb7b58cb38c9d48a401721d082b1dacdb
André Fanelli <andrefanelli@email.com>
2024-11-11 23:07:12 -0300

    Desafio conluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone: Iphone");
+Iphone phone1 = new("(00)00000-1111", "Iphone 15 Pro", "123456789012345", 128);
+phone1.Ligar();
+Console.WriteLine($"Seu número é: {phone1.Numero}");
+phone1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n---------------------------------------\n");
+
+Console.WriteLine("Smartphone: Nokia");
+Nokia phone2 = new("(00)11111-2222", "Nokia C21", "23413567890657432", 128);
+phone2.ReceberLigacao();
+Console.WriteLine($"Seu número é: {phone2.Numero}");
+phone2.InstalarAplicativo("Whatsapp");
+
+
